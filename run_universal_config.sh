45e25a59544ec4fe4416cb501c0f160c457de506
mr.Abdrahimov <mr.abdrahimov@gmail.com>
2025-10-28 23:01:04 +0500

    Update run_universal_config.sh

--- run_universal_config.sh
+++ run_universal_config.sh
@@ -12,5 +12,5 @@ else
 	printf "\033[32;1mThis old firmware.\nRecommendation, upgrade firmware to actual release...\nSleep 5 sec...\033[0m\n"
 	sleep 5
 	printf "\033[32;1mRunning old scprit...\033[0m\n"
-	wget --no-check-certificate -O /tmp/universal_config.sh https://raw.githubusercontent.com/routerich/RouterichAX3000_configs/refs/heads/new_awg_podkop/universal_config.sh && chmod +x /tmp/universal_config.sh && /tmp/universal_config.sh $1 $2
+	wget --no-check-certificate -O /tmp/universal_config.sh https://raw.githubusercontent.com/mr-Abdrahimov/openwrt-unblock/refs/heads/main/universal_config.sh && chmod +x /tmp/universal_config.sh && /tmp/universal_config.sh $1 $2
 fi
