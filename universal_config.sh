f49cb8870b7e5ddf487ad5acb5bffd3784daaa73
mr.Abdrahimov <mr.abdrahimov@gmail.com>
2025-10-28 23:17:45 +0500

    Update universal_config.sh

--- universal_config.sh
+++ universal_config.sh
@@ -486,6 +486,7 @@ install_youtubeunblock_packages() {
     VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
     BASE_URL="https://github.com/Waujito/youtubeUnblock/releases/download/v1.1.0/"
   	PACK_NAME="youtubeUnblock"
+    echo "111111111"
 
     AWG_DIR="/tmp/$PACK_NAME"
     mkdir -p "$AWG_DIR"
@@ -514,7 +515,6 @@ install_youtubeunblock_packages() {
 
         YOUTUBEUNBLOCK_FILENAME="youtubeUnblock-1.1.0-2-2d579d5-aarch64_cortex-a53-openwrt-23.05.ipk"
         DOWNLOAD_URL="${BASE_URL}${YOUTUBEUNBLOCK_FILENAME}"
-        echo "111111111"
 		echo $DOWNLOAD_URL
         wget -O "$AWG_DIR/$YOUTUBEUNBLOCK_FILENAME" "$DOWNLOAD_URL"
 
